adb8cb1ebd13d71029082df6bbb7a163058fd3de
atiwari0209 <148536219+atiwari0209@users.noreply.github.com>
2023-10-20 14:28:03 +0530

    Assignment

---
+++ Assignment.java
@@ -0,0 +1,58 @@
+package SeleniumProject.Webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.*;
+import org.json.JSONObject;
+import org.json.JSONArray;  
+
+import java.util.List;
+
+public class Assignment {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Selenium\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+		driver.get("https://testpages.herokuapp.com/styled/tag/dynamic-table.html");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("/html/body/div/div[3]/details/summary")).click();
+        
+        String jsonData = "[{\"name\" : \"Bob\", \"age\" : 20, \"gender\": \"male\"}, {\"name\": \"George\", \"age\" : 42, \"gender\": \"male\"}, {\"name\":\r\n"
+        		+ "\"Sara\", \"age\" : 42, \"gender\": \"female\"}, {\"name\": \"Conor\", \"age\" : 40, \"gender\": \"male\"}, {\"name\":\r\n"
+        		+ "\"Jennifer\", \"age\" : 42, \"gender\": \"female\"}]";
+        JSONArray jsonArray = new JSONArray(jsonData);
+        
+        
+        driver.findElement(By.xpath("//*[@id=\"jsondata\"]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"jsondata\"]")).sendKeys(jsonData);
+        driver.findElement(By.id("refreshtable")).click();
+        //assertEquals(0, 0, 0)
+        
+        // Grab the table
+        WebElement table = driver.findElement(By.id("dynamictable"));
+
+        // Now get all the TR elements from the table
+        List<WebElement> allRows = table.findElements(By.tagName("tr"));
+        // And iterate over them, getting the cells
+        for(int i=1;i<allRows.size();i++) {
+        	JSONObject rowJson = jsonArray.getJSONObject(i-1);
+        	String[] rowDataValue = {rowJson.getString("name"), String.valueOf(rowJson.getInt("age")),rowJson.getString("gender")};
+        	WebElement currentRow = allRows.get(i);
+        	List<WebElement> cells = currentRow.findElements(By.tagName("td"));
+        	for(int j=0;j<cells.size();j++) {
+        		WebElement cell = cells.get(j);
+        		String cellText = cell.getText();
+        		System.out.print(cellText + " ");
+        		//assertEquals(rowDataValue[j], cellText);
+        		try {
+        			assertEquals(rowDataValue[j], cellText);
+        		} catch(AssertionError error) {
+        			System.out.println(error.getMessage());
+        		}
+        	}
+        	System.out.println();
+        }
+        
+}
+}
